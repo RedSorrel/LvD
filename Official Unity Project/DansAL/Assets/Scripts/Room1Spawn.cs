57e5cabee24e45f4c938cf7ace32d0d42de50dd0
Daniel Marifjeren <marifjeren1@yahoo.com>
2015-03-13 14:14:09 -0400

    Daniels Assets and Library Yo
    This is what im doingggg

---
+++ Official Unity Project/DansAL/Assets/Scripts/Room1Spawn.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room1Spawn : MonoBehaviour {
+
+	public GameObject [] spawnItems = new GameObject[4];
+	public static int howManyPickUps;
+	public static int hasBeenUsed; 
+	public aimDot id; 
+
+	// Use this for initialization
+
+
+	
+	/*void OnLevelWasLoaded(int level) {
+		if(level==1)
+			Debug.Log ("YO2"); 
+		
+	}*/
+
+	void Start () {
+		//lets us refrence the ID from aimDot script.
+		aimDot id = GetComponent<aimDot>();
+			setUpPickups();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if ID !=0 it means we are hovering over an object.
+		if(Input.GetKeyDown ("f")&& aimDot.ID != 0)
+		{
+			pickUpItem();
+		}
+	}
+
+	void setUpPickups(){
+		//sets all items in array with a value of "False" to not be active anymore//
+		for(int i = 0; i< SpawnManager.room1Pickups.Length; i++)
+		{
+			if(SpawnManager.room1Pickups[i] == false)
+			{
+				spawnItems[i].SetActive(false); 
+			}
+
+		}
+
+	}
+
+
+
+	public void pickUpItem(){
+
+		int test = aimDot.ID; 
+
+		//loops through array of items, if it finds a match to the id it will delete the item in the array at i, then set
+		//pickups so it wont render anymore
+		for(int i = 0; i< spawnItems.Length; i++)
+		{
+			if(spawnItems[i].gameObject.GetInstanceID() == test)
+			{
+				Debug.Log ("ITS AT INDEX: "+i); 
+				SpawnManager.deleteItem(i); 
+				setUpPickups (); 
+			}
+			
+		}
+	}
+}
---
+++ Official Unity Project/DansAL/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomManager : MonoBehaviour {
+	//put number of lights here
+	public static bool [] arrayOfLight = new bool[3];
+
+	void Awake(){
+		DontDestroyOnLoad (transform.gameObject); 
+	}
+
+	void Start () {
+		setUpStartingLight(); 
+
+	}
+
+
+	//Starts declares random t/f for each room at start of game. 
+	void setUpStartingLight(){
+		for(int i = 0; i < arrayOfLight.Length;i++){
+			int number = Random.Range (1,3);
+			if(number == 1)
+				arrayOfLight[i] = true;
+			else
+				arrayOfLight[i] = false; 
+		}
+		//after it sets up lights it will load the first room.
+		 Application.LoadLevel ("Room1"); 
+	}
+
+	//Each room has an index in the light array, once it sends the room number the
+	//function will select that index and set it to false. 
+	public static void changeLight(int room)
+	{
+		if(arrayOfLight[room] == true)
+			arrayOfLight[room] = false; 
+		else
+			arrayOfLight[room] = true; 
+	}
+
+
+	//lets room know if light is on or off
+	public static bool isLightOn(int room)
+	{
+		return arrayOfLight[room]; 
+	}
+}
